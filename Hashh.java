2215f205d11e90bd21a203fb8075c1872211f6ca
macuelo <bassflea123123@gmail.com>
2025-10-03 09:41:05 -0500

    Busqueda por Hashh

--- Hashh.java
+++ Hashh.java
@@ -1,30 +1,52 @@
 
 package hashh;
 import java.util.List;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+
 public class Hashh {
+    
+private static final int NUM_BUCKETS = 5;
+    private List<String>[] buckets;
 
-    public static void main(String[] args) {
-
-        List<String> datos = Arrays.asList(
-            "CS2", "Dota", "AOE", "DOOM", "Portal"
-        );
-        
-        Map<String, Integer> indice = new HashMap<>();
-        for (int i = 0; i < datos.size(); i++) {
-            indice.put(datos.get(i), i);}
-        
-        String clave = "cs";
-        if (indice.containsKey(clave)) {
-            int posicion = indice.get(clave);
-            System.out.println("Encontrado: " + datos.get(posicion) + 
-                               " en posicion " + posicion);
-        } else {
-            System.out.println("No encontrado");
+    @SuppressWarnings("unchecked")
+    public Hashh() {
+        buckets = new List[NUM_BUCKETS];
+        for (int i = 0; i < NUM_BUCKETS; i++) {
+            buckets[i] = new ArrayList<>();
         }
+    }
+
+    
+    private int hash(String clave) {
+        return Math.abs(clave.hashCode()) % NUM_BUCKETS;
+    }
+
+    public void insertar(String clave) {
+        int bucketId = hash(clave);
+        buckets[bucketId].add(clave);
+        System.out.println("Insertado '" + clave + "' en bucket " + bucketId);
+    }
+
+    public boolean buscar(String clave) {
+        int bucketId = hash(clave);
+        System.out.println("Buscando en bucket " + bucketId);
+        return buckets[bucketId].contains(clave);
+    }
+    
+    
+    
+    public static void main(String[] args)  {
+        Hashh hashTable = new Hashh();
         
+        // Insertar datos
+        hashTable.insertar("Dota");
+        hashTable.insertar("CS2");
+        hashTable.insertar("AOE");
+        hashTable.insertar("Sekiro");
+
+        // Buscar
+        System.out.println("¿'Dota' esta? " + hashTable.buscar("Dota"));
+        System.out.println("¿'DOOM' esta? " + hashTable.buscar("Doom"));
     }
 }
         
